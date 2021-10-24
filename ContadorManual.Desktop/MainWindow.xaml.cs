4735f02fffd8608f566d825991fd277367354160
Daniel Jesus Pelagio Puch <19470231@campeche.tecnm.mx>
2021-10-24 01:16:40 -0500

    El conteo se realiza utilizando una instancia de la clase contador.

--- ContadorManual.Desktop/MainWindow.xaml.cs
+++ ContadorManual.Desktop/MainWindow.xaml.cs
@@ -1,31 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+namespace ContadorManual.Desktop.Model;
+
 
 namespace ContadorManual.Desktop
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+   
     public partial class MainWindow : Window
     {
         private int _Conteo;
+        private Contador contador;
 
         public MainWindow()
         {
             InitializeComponent();
-            _Conteo = 0;
+                
         }
 
         private void ReiniciarButton_Click(object sender, RoutedEventArgs e)
---
+++ ContadorManual.Desktop/Model/Contador.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContadorManual.Desktop.Model
+{
+    public class Contador
+    {
+        public Contador()
+        {
+            conteoInicial = 0;
+            Contar = _Conteo;
+            Reiniciar = 0;
+
+        }
+        public int _Conteo;
+        public int conteoInicial;
+        public int Contar;
+        public int Reiniciar;
+        
+        public int Conteo
+        {
+            get => _Conteo;
+            set
+            {
+                if (_Conteo != value == false)
+                {
+                    _Conteo = value;
+                }
+            }
+        }
+
+
+    }
+
+    
+}
